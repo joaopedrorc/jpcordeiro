4a10107355dda864af2dafd6dead7445f4aea410
João Pedro Cordeiro <jpc5626@gmail.com>
2024-01-17 21:02:36 -0300

    config

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-/** @type {import('@sveltejs/kit').Config} */
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/joaopedrorc' : ''
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
-	},
-	preprocess: vitePreprocess()
+	}
 };
 
 export default config;
